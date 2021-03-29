b57a273a4243e8f778373ca676dc64e1cf4378b2
Ryan Griffin <ryanjoegriffin@gmail.com>
2021-03-29 16:00:29 -0700

    updated GNode to use ArrayList instead of array (GNode[])

--- src/chapter04/GNode.java
+++ src/chapter04/GNode.java
@@ -1,34 +1,48 @@
 package chapter04;
+
+import java.util.ArrayList;
+
 public class GNode
 {
 	private int data;
-	private GNode[] adjacent;
-	private int adjacentCount;
+	// private GNode[] adjacent;
+	private ArrayList<GNode> adjacent;
 	public State state;
 	
 	public enum State {
 		Unvisited, Visited, Visiting;
 	}
 	
-	public GNode(int val, int adLength)
+	public GNode(int val)
 	{
 		data = val;
-		adjacentCount = 0;
-		adjacent = new GNode[adLength];
+		adjacent = new ArrayList<GNode>();
+		// adjacentCount = 0;
+		// adjacent = new GNode[adLength];
 	}
 	
 	public void addAdjacaent(GNode n)
 	{
-		if(adjacentCount < adjacent.length)
-		{
-			adjacent[adjacentCount] = n;
-			adjacentCount++;
-		}
-		else
-			System.out.println("No more adjacent can be added!");
+		adjacent.add(n);
+		
+//		if(adjacentCount < adjacent.size())
+//		{
+//			adjacent.add(n);
+//			adjacentCount++;
+//		}
+//		else
+//			System.out.println("No more adjacent can be added!");
 	}
 	
-	public GNode[] getAdjacent()
+	public void removeAdjacent(GNode n)
+	{
+		if(adjacent.remove(n))
+			return;
+		else
+			System.out.println("error: node not present!");
+	}
+	
+	public ArrayList<GNode> getAdjacent()
 	{ return adjacent; }
 	
 	public int getData()
